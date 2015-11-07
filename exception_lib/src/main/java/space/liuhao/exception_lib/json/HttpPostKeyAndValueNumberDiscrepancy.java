e6c2ab9828d382467a06b4933623aaba393570d2
liuhao <liuhaoduoduo@163.com>
2015-11-07 16:36:20 +0800

    新增：http请求Post模式中Key与Value数量不一致异常

---
+++ exception_lib/src/main/java/space/liuhao/exception_lib/json/HttpPostKeyAndValueNumberDiscrepancy.java
@@ -0,0 +1,10 @@
+package space.liuhao.exception_lib.json;
+
+/**
+ * http请求Post模式中Key与Value数量不一致异常
+ *
+ * @author 刘浩 2015-11-07 16:31
+ * @version 1.0.0
+ */
+public class HttpPostKeyAndValueNumberDiscrepancy extends Throwable{
+}
