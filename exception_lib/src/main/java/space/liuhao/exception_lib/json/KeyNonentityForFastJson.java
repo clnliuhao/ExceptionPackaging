333850dc89ecc5e007fc92f82c63f01129f5516d
liuhao <liuhaoduoduo@163.com>
2015-11-08 18:10:42 +0800

    选择：构造方法复写

--- exception_lib/src/main/java/space/liuhao/exception_lib/json/KeyNonentityForFastJson.java
+++ exception_lib/src/main/java/space/liuhao/exception_lib/json/KeyNonentityForFastJson.java
@@ -6,11 +6,22 @@ import com.alibaba.fastjson.JSONException;
 /**
  * Key不存在异常封装-基于FastJson
  *
- * @author 刘浩 2015-10-25 11:44
- * @version 1.0.0
+ * @author 刘浩 2015-11-08 18:10:27
+ * @version 1.1.0
  */
 public class KeyNonentityForFastJson extends JSONException implements I_KeyNonentity{
     public KeyNonentityForFastJson(String s){
         super(JSON_EXCEPTION_MESSAGE_KEY + s + JSON_EXCEPTION_MESSAGE_NONENTITY);
+    }
+
+    public KeyNonentityForFastJson(KeyNonentityForFastJson pKeyNonentityForFastJson){
+        super(pKeyNonentityForFastJson.getMessage());
+    }
+
+    public KeyNonentityForFastJson(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public KeyNonentityForFastJson(){
     }
 }
--- exception_lib/src/main/java/space/liuhao/exception_lib/json/KeyNonentityForOrgJson.java
+++ exception_lib/src/main/java/space/liuhao/exception_lib/json/KeyNonentityForOrgJson.java
@@ -6,11 +6,16 @@ import org.json.JSONException;
 /**
  * Key不存在异常封装-基于FastJson
  *
- * @author 刘浩 2015-10-25 11:44
- * @version 1.0.0
+ * @author 刘浩 2015-11-08 18:09:49
+ * @version 1.1.0
  */
 public class KeyNonentityForOrgJson extends JSONException implements I_KeyNonentity{
     public KeyNonentityForOrgJson(String s){
         super(JSON_EXCEPTION_MESSAGE_KEY + s + JSON_EXCEPTION_MESSAGE_NONENTITY);
     }
+
+    public KeyNonentityForOrgJson(KeyNonentityForOrgJson pKeyNonentityForOrgJson){
+        super(pKeyNonentityForOrgJson.getMessage());
+    }
+
 }
