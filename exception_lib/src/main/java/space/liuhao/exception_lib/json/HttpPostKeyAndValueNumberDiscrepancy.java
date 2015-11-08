4c78a69539fe08a2cb5dcdaa090e2afd120c189f
liuhao <liuhaoduoduo@163.com>
2015-11-08 18:06:42 +0800

    选择：构造方法复写

--- exception_lib/src/main/java/space/liuhao/exception_lib/json/HttpPostKeyAndValueNumberDiscrepancy.java
+++ exception_lib/src/main/java/space/liuhao/exception_lib/json/HttpPostKeyAndValueNumberDiscrepancy.java
@@ -3,8 +3,26 @@ package space.liuhao.exception_lib.json;
 /**
  * http请求Post模式中Key与Value数量不一致异常
  *
- * @author 刘浩 2015-11-07 16:31
- * @version 1.0.0
+ * @author 刘浩 2015-11-08 18:05:47
+ * @version 1.1.0
  */
 public class HttpPostKeyAndValueNumberDiscrepancy extends Throwable{
+    public HttpPostKeyAndValueNumberDiscrepancy(){
+    }
+
+    public HttpPostKeyAndValueNumberDiscrepancy(Throwable cause){
+        super(cause);
+    }
+
+    public HttpPostKeyAndValueNumberDiscrepancy(String detailMessage){
+        super(detailMessage);
+    }
+
+    public HttpPostKeyAndValueNumberDiscrepancy(String detailMessage, Throwable cause){
+        super(detailMessage, cause);
+    }
+
+    public HttpPostKeyAndValueNumberDiscrepancy(String detailMessage, Throwable cause, boolean enableSuppression, boolean writableStackTrace){
+        super(detailMessage, cause, enableSuppression, writableStackTrace);
+    }
 }
--- exception_lib/src/main/java/space/liuhao/exception_lib/json/ParameterNotIsNullOrEmpty.java
+++ exception_lib/src/main/java/space/liuhao/exception_lib/json/ParameterNotIsNullOrEmpty.java
@@ -3,11 +3,27 @@ package space.liuhao.exception_lib.json;
 /**
  * 参数不可以为Null或空字符串异常
  *
- * @author 刘浩 2015-11-07 16:25
- * @version 1.0.0
+ * @author 刘浩 2015-11-08 18:05:33
+ * @version 1.1.0
  */
 public class ParameterNotIsNullOrEmpty extends Throwable{
 
     public ParameterNotIsNullOrEmpty(){
+    }
+
+    public ParameterNotIsNullOrEmpty(Throwable cause){
+        super(cause);
+    }
+
+    public ParameterNotIsNullOrEmpty(String detailMessage){
+        super(detailMessage);
+    }
+
+    public ParameterNotIsNullOrEmpty(String detailMessage, Throwable cause){
+        super(detailMessage, cause);
+    }
+
+    public ParameterNotIsNullOrEmpty(String detailMessage, Throwable cause, boolean enableSuppression, boolean writableStackTrace){
+        super(detailMessage, cause, enableSuppression, writableStackTrace);
     }
 }
